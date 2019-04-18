fdb9337260a81d61dc488c798c92034388b24dc3
jsmolka <jsmolka@pm.me>
2019-04-18 09:48:32 +0200

    implement key input

--- egg-vance/src/core.cpp
+++ egg-vance/src/core.cpp
@@ -1,13 +1,13 @@
 #include "core.h"
 
+#include "common/memory_map.h"
+
 Core::Core()
     : arm(mmu)
 {
     ppu.mmu = &mmu;
 
-    arm.reset();
-    mmu.reset();
-    ppu.reset();
+    reset();
 }
 
 void Core::run(const std::string& file)
@@ -20,10 +20,12 @@ void Core::run(const std::string& file)
     SDL_Event event;
     while (running)
     {
-        for (int i = 0; i < 2048; ++i)
+        for (int i = 0; i < 12500; ++i)
             arm.step(); 
 
         ppu.renderFrame();
+
+        u16 keyinput = mmu.readHalf(REG_KEYINPUT);
 
         while (SDL_PollEvent(&event))
         {
@@ -36,8 +38,43 @@ void Core::run(const std::string& file)
             case SDL_KEYDOWN:
                 if (event.key.keysym.sym == SDLK_ESCAPE)
                     running = false;
+                else
+                    keyinput |= keyMask(event.key.keysym.sym);
+                break;
+
+            case SDL_KEYUP:
+                keyinput &= ~keyMask(event.key.keysym.sym);
                 break;
             }
         }
+
+        mmu.writeHalf(REG_KEYINPUT, keyinput);
     }
+}
+
+void Core::reset()
+{
+    arm.reset();
+    mmu.reset();
+    ppu.reset();
+
+    mmu.writeHalf(REG_KEYINPUT, 0xFF);
+}
+
+u16 Core::keyMask(const SDL_Keycode& key)
+{
+    switch (key)
+    {
+    case SDLK_u: return 1 << 0;  // Button A
+    case SDLK_h: return 1 << 1;  // Button B
+    case SDLK_f: return 1 << 2;  // Select
+    case SDLK_g: return 1 << 3;  // Start
+    case SDLK_d: return 1 << 4;  // Right
+    case SDLK_a: return 1 << 5;  // Left
+    case SDLK_w: return 1 << 6;  // Up
+    case SDLK_s: return 1 << 7;  // Down
+    case SDLK_i: return 1 << 8;  // Button R
+    case SDLK_q: return 1 << 9;  // Button L
+    }
+    return 0;
 }
--- egg-vance/src/core.h
+++ egg-vance/src/core.h
@@ -2,6 +2,8 @@
 
 #include <string>
 
+#include <SDL_events.h>
+
 #include "arm/arm.h"
 #include "mmu/mmu.h"
 #include "ppu/ppu.h"
@@ -14,6 +16,10 @@ public:
     void run(const std::string& file);
 
 private:
+    void reset();
+
+    u16 keyMask(const SDL_Keycode& key);
+
     MMU mmu;
     PPU ppu;
     ARM arm;
