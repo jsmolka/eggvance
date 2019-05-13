e89b1250ae74e4487679d446636bbfca789c482b
jsmolka <jsmolka@pm.me>
2019-05-13 12:52:10 +0200

    implement tile overflow

--- egg-vance/src/ppu/render_sprites.cpp
+++ egg-vance/src/ppu/render_sprites.cpp
@@ -3,7 +3,6 @@
 #include "mmu/map.h"
 #include "sprites/oamentry.h"
 
-// Todo: handle tile "overflow" 18000h -> 10000h
 void PPU::updateSprites()
 {
     for (int entry = 0; entry < 128; ++entry)
@@ -26,49 +25,56 @@ void PPU::updateSprites()
         if (sprite.x >= WIDTH)  sprite.x -= 512;
         if (sprite.y >= HEIGHT) sprite.y -= 256;
 
-        sprite.width  = oam.spriteWidth();
-        sprite.height = oam.spriteHeight();
+        sprite.width   = oam.spriteWidth();
+        sprite.height  = oam.spriteHeight();
         sprite.palette = oam.attr2.palette;
 
-        int tile_y_offset = 0x20 * (mmu.dispcnt.sprite_1d ? (sprite.width / 8) : 32);
+        u32 tile_addr = MAP_VRAM + 0x10000 + 0x20 * oam.attr2.tile;
 
-        u32 base_tile = MAP_VRAM + 0x10000 + 0x20 * oam.attr2.tile;
-
-        // Current sprite coordinates
-        int sprite_y = 0;
-
+        int base_y = 0;
         // Iterate vertical tiles
         for (int tile_y = 0; tile_y < (sprite.height / 8); ++tile_y)
         {
-            int sprite_x = 0;
-
-            // Offset in current tile
-            int offset = 0;
-
+            int base_x = 0;
             // Iterate horizontal tiles
             for (int tile_x = 0; tile_x < (sprite.width / 8); ++tile_x)
             {
                 // Iterate vertical tile pixels
                 for (int y = 0; y < 8; ++y)
                 {
-                    // Iterate horizontal tile pixels (2 pixel per byte)
+                    // Iterate horizontal tile pixels (2 pixels per byte)
                     for (int x = 0; x < 4; ++x)
                     {
-                        int byte = mmu.readByteFast(base_tile + offset);
-                        int value1 = byte & 0xF;
-                        int value2 = byte >> 4;
+                        int byte = mmu.readByteFast(tile_addr);
 
-                        sprite.setPixel(sprite_x + 2 * x, sprite_y + y, value1, oam.attr1.flip_x, oam.attr1.flip_y);
-                        sprite.setPixel(sprite_x + 2 * x + 1, sprite_y + y, value2, oam.attr1.flip_x, oam.attr1.flip_y);
+                        sprite.setPixel(
+                            base_x + 2 * x,
+                            base_y + y,
+                            byte & 0xF,
+                            oam.attr1.flip_x, 
+                            oam.attr1.flip_y
+                        );
+                        sprite.setPixel(
+                            base_x + 2 * x + 1, 
+                            base_y + y, 
+                            byte >> 4,
+                            oam.attr1.flip_x, 
+                            oam.attr1.flip_y
+                        );
 
-                        offset++;
+                        tile_addr++;
                     }
                 }
-                sprite_x += 8;
-            }
-            sprite_y += 8;
+                base_x += 8;
 
-            base_tile += tile_y_offset;
+                // Tiles "overflow" if the last one is reached
+                if (tile_addr > (MAP_VRAM + 0x18000))
+                    tile_addr -= 0x8000;
+            }
+            base_y += 8;
+
+            if (!mmu.dispcnt.sprite_1d)
+                tile_addr += 0x20 * 24;
         }
     }
 }
