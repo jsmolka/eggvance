98c8127c64a6b3d93a22f4899234e7990abc7f07
jsmolka <jsmolka@pm.me>
2019-12-31 16:21:57 +0100

    make clock cross platform

--- eggvance/src/microclock.cpp
+++ eggvance/src/microclock.cpp
@@ -2,7 +2,14 @@
 
 // Based on https://stackoverflow.com/a/41862592
 
-#include <windows.h>
+#ifdef _MSC_VER
+#  include <windows.h>
+#else
+#  include <time.h>
+#  include <errno.h>
+#endif
+
+#ifdef _MSC_VER
 
 class TimerHandle
 {
@@ -58,3 +65,23 @@ void micro_clock::sleep(u32 us)
     SetWaitableTimer(timer, &relative_time, 0, NULL, NULL, 0);
     WaitForSingleObject(timer, INFINITE);
 }
+
+#else
+
+u64 micro_clock::now()
+{
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    return static_cast<u64>(now.tv_sec) * 1000000 + now.tv_nsec / 1000;
+}
+
+void micro_clock::sleep(u32 us)
+{
+    struct timespec ts;
+    ts.tv_sec = us / 1000000;
+    ts.tv_nsec = us % 1000000 * 1000;
+
+    while (nanosleep(&ts, &ts) == -1 && errno == EINTR);
+}
+
+#endif
