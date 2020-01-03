3d14b43612ff2841c8770b82e3f1705850c16da3
jsmolka <jsmolka@pm.me>
2020-01-03 12:57:09 +0100

    project file changes

--- eggvance/src/common/macros.h
+++ eggvance/src/common/macros.h
@@ -2,18 +2,14 @@
 
 #include <cassert>
 
-#ifdef EGG_DEBUG
-#  define EGG_ASSERT(cond, msg) assert((cond) && msg)
-#else
-#  define EGG_ASSERT(cond, msg) static_cast<void>(0)
-#endif
+#define EGG_ASSERT(cond, msg) assert((cond) && msg)
 
-#ifdef EGG_DEBUG
-#  define EGG_UNREACHABLE EGG_ASSERT(false, "Unreachable")
-#else
+#ifdef NDEBUG
 #  ifdef _MSC_VER
 #    define EGG_UNREACHABLE __assume(0)
 #  else
 #    define EGG_UNREACHABLE static_cast<void>(0)
 #  endif
+#else
+#  define EGG_UNREACHABLE EGG_ASSERT(false, "Unreachable")
 #endif
