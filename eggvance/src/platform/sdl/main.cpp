904a385e5193a43d531c00548f5c0c27b39e9939
jsmolka <jsmolka@pm.me>
2020-01-12 18:23:20 +0100

    fix main

--- eggvance/src/platform/sdl/main.cpp
+++ eggvance/src/platform/sdl/main.cpp
@@ -2,7 +2,13 @@
 
 #include "sdlplatform.h"
 
-int SDL_main(int argc, char* argv[])
+#ifdef _MSC_VER
+#define EGG_MAIN SDL_main
+#else
+#define EGG_MAIN main
+#endif
+
+int EGG_MAIN(int argc, char* argv[])
 {
     SDLPlatform platform;
     try
