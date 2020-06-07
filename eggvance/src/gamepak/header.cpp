fe85688019ec30eaee32f42f76b9c5750e0876c3
jsmolka <jsmolka@pm.me>
2020-06-07 13:05:18 +0200

    parse header only if valid

--- eggvance/src/gamepak/header.cpp
+++ eggvance/src/gamepak/header.cpp
@@ -3,10 +3,10 @@
 #include <algorithm>
 #include <numeric>
 
-template<uint N>
-static std::string makePrintable(u8(&data)[N])
+template<std::size_t N>
+static std::string makePrintable(const u8 (&data)[N])
 {
-    std::string str(reinterpret_cast<char*>(data), N);
+    std::string str(reinterpret_cast<const char*>(data), N);
     str.erase(std::find(str.begin(), str.end(), '\0'), str.end());
 
     return str;
@@ -14,21 +14,22 @@ static std::string makePrintable(u8(&data)[N])
 
 Header::Header(const std::vector<u8>& rom)
 {
-    if (rom.size() < sizeof(HeaderData))
+    if (rom.size() < sizeof(Data))
         return;
 
-    std::copy_n(rom.begin(), sizeof(HeaderData), reinterpret_cast<u8*>(&header));
+    const Data& data = *reinterpret_cast<const Data*>(rom.data());
 
-    title = makePrintable(header.game_title);
-    code  = makePrintable(header.game_code);
-
-    u8 expected = complement();
+    if (data.fixed_96h == 0x96 && data.complement == complement(data))
+    {
+        title = makePrintable(data.game_title);
+        code  = makePrintable(data.game_code);
+    }
 }
 
-u8 Header::complement() const
+u8 Header::complement(const Data& data)
 {
-    const u8* beg = reinterpret_cast<const u8*>(&header) + 0xA0;
-    const u8* end = reinterpret_cast<const u8*>(&header) + 0xBD;
+    const u8* beg = reinterpret_cast<const u8*>(&data) + 0xA0;
+    const u8* end = reinterpret_cast<const u8*>(&data) + 0xBD;
 
     return -(std::accumulate<const u8*, u8>(beg, end, 0) + 0x19);
 }
--- eggvance/src/gamepak/header.h
+++ eggvance/src/gamepak/header.h
@@ -9,27 +9,27 @@ class Header
 {
 public:
     Header() = default;
-    Header(const std::vector<u8>& rom);
+    explicit Header(const std::vector<u8>& rom);
 
     std::string title;
     std::string code;
 
 private:
-    u8 complement() const;
-
-    struct HeaderData
+    struct Data
     {
-        u32 branch;
-        u8  logo[156];
+        u32 entry_point;
+        u8  nintendo_logo[156];
         u8  game_title[12];
         u8  game_code[4];
         u8  maker_code[2];
-        u8  fixed;
+        u8  fixed_96h;
         u8  unit_code;
         u8  device_type;
-        u8  unused[7];
+        u8  reserved_area[7];
         u8  game_version;
         u8  complement;
         u16 checksum;
-    } header = { 0 };
+    };
+
+    static u8 complement(const Data& data);
 };
