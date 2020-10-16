bbac8ebbc9e302e05ec1d9285daefcf63ac02068
jsmolka <jsmolka@pm.me>
2020-10-16 21:16:35 +0200

    add comparison operators to point

--- eggvance/src/gpu/point.h
+++ eggvance/src/gpu/point.h
@@ -12,40 +12,47 @@ struct Point
         return width * y + x;
     }
 
-    #define OPERATOR(op)                                        \
-        constexpr Point operator##op(int scalar) const          \
-        {                                                       \
-            return Point(x ##op scalar, y ##op scalar);         \
-        }                                                       \
-                                                                \
-        constexpr Point operator##op(const Point& other) const  \
-        {                                                       \
-            return Point(x ##op other.x, y ##op other.y);       \
-        }                                                       \
-                                                                \
-        constexpr Point& operator##op##=(int scalar)            \
-        {                                                       \
-            x ##op##= scalar;                                   \
-            y ##op##= scalar;                                   \
-            return *this;                                       \
-        }                                                       \
-                                                                \
-        constexpr Point& operator##op##=(const Point& other)    \
-        {                                                       \
-            x ##op##= other.x;                                  \
-            y ##op##= other.y;                                  \
-            return *this;                                       \
+    constexpr bool operator==(const Point& other) const { return x == other.x && y == other.y; }
+    constexpr bool operator!=(const Point& other) const { return x != other.x || y != other.y; }
+    constexpr bool operator< (const Point& other) const { return x <  other.x && y <  other.y; }
+    constexpr bool operator> (const Point& other) const { return x >  other.x && y >  other.y; }
+    constexpr bool operator<=(const Point& other) const { return x <= other.x && y <= other.y; }
+    constexpr bool operator>=(const Point& other) const { return x >= other.x && y >= other.y; }
+
+    #define POINT_OPERATOR(op)                                 \
+        constexpr Point operator op(int scalar) const          \
+        {                                                      \
+            return Point(x op scalar, y op scalar);            \
+        }                                                      \
+                                                               \
+        constexpr Point operator op(const Point& other) const  \
+        {                                                      \
+            return Point(x op other.x, y op other.y);          \
+        }                                                      \
+                                                               \
+        constexpr Point& operator op##=(int scalar)            \
+        {                                                      \
+            x op##= scalar;                                    \
+            y op##= scalar;                                    \
+            return *this;                                      \
+        }                                                      \
+                                                               \
+        constexpr Point& operator op##=(const Point& other)    \
+        {                                                      \
+            x op##= other.x;                                   \
+            y op##= other.y;                                   \
+            return *this;                                      \
         }
 
-    OPERATOR(+)
-    OPERATOR(-)
-    OPERATOR(*)
-    OPERATOR(/)
-    OPERATOR(%)
-    OPERATOR(<<)
-    OPERATOR(>>)
+    POINT_OPERATOR(+)
+    POINT_OPERATOR(-)
+    POINT_OPERATOR(*)
+    POINT_OPERATOR(/)
+    POINT_OPERATOR(%)
+    POINT_OPERATOR(<<)
+    POINT_OPERATOR(>>)
 
-    #undef OPERATOR
+    #undef POINT_OPERATOR
 
     int x = 0;
     int y = 0;
