b6e842102785539ae0fa0a1b5c4f43382f61c895
jsmolka <jsmolka@pm.me>
2020-11-29 19:28:57 +0100

    clean up cycle calculation

--- eggvance/src/arm/io.cpp
+++ eggvance/src/arm/io.cpp
@@ -20,30 +20,32 @@ WaitControl::WaitControl()
 
 void WaitControl::update()
 {
-    static constexpr int kNonSeq[4] = { 4, 3, 2, 8 };
-    static constexpr int kWs0Seq[2] = { 2, 1 };
-    static constexpr int kWs1Seq[2] = { 4, 1 };
-    static constexpr int kWs2Seq[2] = { 8, 1 };
+    static constexpr int kNonSeq[4] = { 5, 4, 3, 9 };
+    static constexpr int kWs0Seq[2] = { 3, 2 };
+    static constexpr int kWs1Seq[2] = { 5, 2 };
+    static constexpr int kWs2Seq[2] = { 9, 2 };
+
+    constexpr uint kN = static_cast<uint>(Access::NonSequential);
+    constexpr uint kS = static_cast<uint>(Access::Sequential);
 
     for (uint x = 0; x < 2; ++x)
     {
-        cycles_half[0x8 + x][0] = kNonSeq[ws0_n] + 1;
-        cycles_half[0xA + x][0] = kNonSeq[ws1_n] + 1;
-        cycles_half[0xC + x][0] = kNonSeq[ws2_n] + 1;
+        cycles_half[0x8 + x][kN] = kNonSeq[ws0_n];
+        cycles_word[0x8 + x][kN] = kNonSeq[ws0_n] + kWs0Seq[ws0_s];
+        cycles_half[0xA + x][kN] = kNonSeq[ws1_n];
+        cycles_word[0XA + x][kN] = kNonSeq[ws1_n] + kWs1Seq[ws1_s];
+        cycles_half[0xC + x][kN] = kNonSeq[ws2_n];
+        cycles_word[0xC + x][kN] = kNonSeq[ws2_n] + kWs2Seq[ws2_s];
+        cycles_half[0xE + x][kN] = kNonSeq[sram];
+        cycles_word[0xE + x][kN] = kNonSeq[sram];
         
-        cycles_half[0x8 + x][1] = kWs0Seq[ws0_s] + 1;
-        cycles_half[0xA + x][1] = kWs1Seq[ws1_s] + 1;
-        cycles_half[0xC + x][1] = kWs2Seq[ws2_s] + 1;
-
-        cycles_word[0x8 + x][0] = cycles_half[0x8][0] + cycles_half[0x8][1];
-        cycles_word[0XA + x][0] = cycles_half[0XA][0] + cycles_half[0XA][1];
-        cycles_word[0xC + x][0] = cycles_half[0xC][0] + cycles_half[0xC][1];
-        
-        cycles_word[0x8 + x][1] = 2 * cycles_half[0x8][1];
-        cycles_word[0XA + x][1] = 2 * cycles_half[0XA][1];
-        cycles_word[0xC + x][1] = 2 * cycles_half[0xC][1];
-
-        cycles_half[0xE + x][x] = kNonSeq[sram] + 1;
-        cycles_word[0xE + x][x] = kNonSeq[sram] + 1;
+        cycles_half[0x8 + x][kS] = kWs0Seq[ws0_s];
+        cycles_word[0x8 + x][kS] = kWs0Seq[ws0_s] * 2;
+        cycles_half[0xA + x][kS] = kWs1Seq[ws1_s];
+        cycles_word[0XA + x][kS] = kWs1Seq[ws1_s] * 2;
+        cycles_half[0xC + x][kS] = kWs2Seq[ws2_s];
+        cycles_word[0xC + x][kS] = kWs2Seq[ws2_s] * 2;
+        cycles_half[0xE + x][kS] = kNonSeq[sram];
+        cycles_word[0xE + x][kS] = kNonSeq[sram];
     }
 }
