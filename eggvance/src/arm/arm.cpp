3ee63e12aeda10a9ff25efce002eacbc2b15ecc1
jsmolka <jsmolka@pm.me>
2020-11-30 21:10:23 +0100

    always use seq and nonseq cycles for pipeline reload

--- eggvance/src/arm/arm.cpp
+++ eggvance/src/arm/arm.cpp
@@ -45,18 +45,18 @@ void Arm::run(int cycles)
 void Arm::flushHalf()
 {
     pc &= ~0x1;
-    pipe[0] = readHalf(pc + 0, pipe.access);
+    pipe[0] = readHalf(pc + 0, Access::NonSequential);
+    pipe[1] = readHalf(pc + 2, Access::Sequential);
     pipe.access = Access::Sequential;
-    pipe[1] = readHalf(pc + 2, pipe.access);
     pc += 2;
 }
 
 void Arm::flushWord()
 {
     pc &= ~0x3;
-    pipe[0] = readWord(pc + 0, pipe.access);
+    pipe[0] = readWord(pc + 0, Access::NonSequential);
+    pipe[1] = readWord(pc + 4, Access::Sequential);
     pipe.access = Access::Sequential;
-    pipe[1] = readWord(pc + 4, pipe.access);
     pc += 4;
 }
 
@@ -95,8 +95,8 @@ void Arm::dispatch()
                         u16 instr = pipe[0];
 
                         pipe[0] = pipe[1];
-                        pipe.access = Access::Sequential;
                         pipe[1] = readHalf(pc, pipe.access);
+                        pipe.access = Access::Sequential;
 
                         std::invoke(instr_thumb[hashThumb(instr)], this, instr);
                     }
@@ -105,8 +105,8 @@ void Arm::dispatch()
                         u32 instr = pipe[0];
 
                         pipe[0] = pipe[1];
-                        pipe.access = Access::Sequential;
                         pipe[1] = readWord(pc, pipe.access);
+                        pipe.access = Access::Sequential;
 
                         if (cpsr.check(instr >> 28))
                         {
@@ -125,6 +125,5 @@ void Arm::dispatch()
 
 void Arm::idle()
 {
-    pipe.access = Access::NonSequential;
     cycles--;
 }
