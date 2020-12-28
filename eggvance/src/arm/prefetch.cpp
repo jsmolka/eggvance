5b355ff0c990f187ba4953de2c56885ad1603e0d
jsmolka <jsmolka@pm.me>
2020-12-28 14:51:03 +0100

    pass more timing tests

--- eggvance/src/arm/prefetch.cpp
+++ eggvance/src/arm/prefetch.cpp
@@ -11,10 +11,11 @@ int Arm::prefetch(u32 addr, int wait)
 
     if (addr < 0x800'0000 || addr >= 0xE00'0000)
     {
-        int seq = waitcnt.cyclesHalf(pc, Access::Sequential);
+        int non = waitcnt.cyclesHalf(pc, Access::NonSequential) - 1;
+        int seq = waitcnt.cyclesHalf(pc, Access::Sequential) - 1;
         int max = 8 * seq;
 
-        prefetched = std::min(max, prefetched + seq);
+        prefetched = std::min(max, prefetched + wait + non - seq);
     }
     else if (prefetched > 0)
     {
