13fd23e4665ce80bbee157e1ccda5c6822f6b370
jsmolka <jsmolka@pm.me>
2020-12-29 20:09:13 +0100

    remove useless range check

--- eggvance/src/arm/prefetch.cpp
+++ eggvance/src/arm/prefetch.cpp
@@ -20,7 +20,7 @@ void Arm::prefetchRom(u32 addr, int cycles)
     if (!waitcnt.prefetch)
         return tick(cycles);
 
-    if (addr >= 0x800'0000 && addr < 0xE00'0000 && prefetch.cycles > 0)
+    if (prefetch.cycles > 0)
     {
         int non = waitcnt.cyclesHalf(addr, Access::NonSequential);
         int seq = waitcnt.cyclesHalf(addr, Access::Sequential);
