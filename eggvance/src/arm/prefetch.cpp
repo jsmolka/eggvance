e10a8d075679fd9da9fbfb2ace31b4c2021a4c0e
jsmolka <jsmolka@pm.me>
2020-12-30 10:11:59 +0100

    minor prefetch changes

--- eggvance/src/arm/prefetch.cpp
+++ eggvance/src/arm/prefetch.cpp
@@ -4,7 +4,7 @@
 
 void Arm::prefetchRam(int cycles)
 {
-    if (waitcnt.prefetch && prefetch.active && !(state & kStateDma))
+    if (waitcnt.prefetch && !(state & kStateDma) && prefetch.active)
         prefetch.cycles += cycles;
 
     tick(cycles);
@@ -16,7 +16,7 @@ void Arm::prefetchRom(u32 addr, int cycles)
     {
         prefetch.active = addr == pc;
 
-        if (prefetch.active && prefetch.cycles > 0)
+        if (prefetch.active && prefetch.cycles)
         {
             int non = waitcnt.cyclesHalf(addr, Access::NonSequential);
             int seq = waitcnt.cyclesHalf(addr, Access::Sequential);
